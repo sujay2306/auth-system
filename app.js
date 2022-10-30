956ac1df1ecb98b18dc1611e388b0f3e2809b768
sujay2306 <sujayks007@gmail.com>
2022-10-30 15:26:32 +0530

    added JWT

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 require("dotenv").config();
 require('./config/database').connect() //connecting to DB
 const express = require("express");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const User = require("./model/user")
 const app = express();
 app.use(express.json());
@@ -18,7 +20,9 @@ app.get("/",(req, res) => {
 
 
 app.post("/register", async (req,res) => {
-    const {firstname, lastname, email, password} = req.body;
+
+    try{
+        const {firstname, lastname, email, password} = req.body;
 
     if(!(email && firstname && lastname && password)){
         res.status(400).send("All fields are required");
@@ -28,6 +32,36 @@ app.post("/register", async (req,res) => {
         if (existingUser){
             res.status(401).send("user already exists")
         }
+    const myEncryptPassword = await bcrypt.hash(password, 10) //how many rounds of hash
+
+    //creating a oobject
+    const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password: myEncryptPassword
+    })
+
+    //token creation
+    const token = jwt.sign(
+        {user_id: user._id, email},//inside db obj name and _id, passing algorithm and when it shuld expire 
+        process.env.SECRET_KEY, 
+        {
+            expiresIn: "2h"
+        }
+    )
+
+    user.token = token
+    //update or not in DB
+    //Handle password situation
+    user.password = undefined
+
+    res.status(201).json(user)
+    }
+    catch(error){
+        console.log(error);
+    }
 });
+
 
 module.exports = app;
